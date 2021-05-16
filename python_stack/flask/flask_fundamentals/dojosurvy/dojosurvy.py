a79c859fc4b4a3d315c08b8b8193864cc2676cbf
yousef <ronorozoroz@hotmail.com>
2021-05-16 14:07:55 +0300

    dojosurvy

---
+++ python_stack/flask/flask_fundamentals/dojosurvy/dojosurvy.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("dojo.html")
+
+@app.route('/result', methods=['POST'])
+def create_user():
+    print(request.form)
+    name_from_form = request.form['name']
+    email_from_form = request.form['email']
+    return render_template("show.html", name_on_template=name_from_form, email_on_template=email_from_form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
